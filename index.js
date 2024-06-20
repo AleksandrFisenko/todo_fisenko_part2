16fee0b05888ef5045dd4f9560be677e1f0318e4
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2024-06-20 16:58:19 +0300

    day 2

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const ENTER = "Enter"
+const ESCAPE = "Escape"
+
 const inputTaskName = document.querySelector("#input-name")
 const buttonAdd = document.querySelector("#button-add-todo")
 const selectAll = document.querySelector("#select-all")
 const taskList = document.querySelector("#task-list")
 const buttonDeleteAll = document.querySelector("#button-delete")
 
-
-const ButtonSortAll = document.querySelector("#button-sort-all")
+const buttonSortAll = document.querySelector("#button-sort-all")
 const sortButtonActive = document.querySelector("#button-sort-active")
 const sortButtonComplited = document.querySelector("#button-sort-complited")
-
-
 
 let taskArray = []
 taskArray.push(
@@ -20,76 +20,119 @@ taskArray.push(
     }
 )
 
-
 const addTask = () => {
-    //console.log(inputTaskName.value)
+  if(inputTaskName.value.trim() !== ""){
     const task = {
-        id: Date.now(),
-        name: inputTaskName.value,
-        isChecked: false,
-    }
-    taskArray.push(task)
-    //console.log(task.id)
-    render()
+      id: Date.now(),
+      name: inputTaskName.value,
+      isChecked: false,
+  }
+  taskArray.push(task)
+  inputTaskName.value = ""
+  render()
+  }
 }
 
 const handleClick = (event) => {
-    console.log(event)
-    const clickedItamName = event.target.classList.value
+    const clickedItemName = event.target.classList.value
     const taskId = event.target.parentElement.id
-    if (clickedItamName == "todo-delete") {
+    if (clickedItemName == "todo-delete") {
         taskArray = taskArray.filter(task => task.id != taskId)
+        render()
     } 
-    if(clickedItamName == "todo-checkbox"){
-      taskArray.forEach(task => {
-        if(taskId == task.id){
-          task.isChecked = event.target.checked
-        }
-      })
+    if(clickedItemName == "todo-checkbox"){
+        taskArray.forEach(task => {
+            if(taskId == task.id){
+                task.isChecked = event.target.checked
+            }
+        })
+        render()
     }
-    
+    if ((event.detail == 2) && (clickedItemName == "todo-text")) {
 
+      event.target.hidden = true
+      event.target.previousElementSibling.hidden = false
+      event.target.previousElementSibling.focus()
+
+    }
+}
+
+const editingTask = (event) => {
+  console.log(event.key)
+  if((event.key == ENTER) && (event.target.value.trim() !== "")){
+    const taskId = event.target.parentElement.id
+    
+    taskArray.forEach(task => {
+      if(task.id == taskId){
+        task.name = event.target.value
+      }
+    })
     render()
+  }
+  if(event.key == ESCAPE){
+    render()
+  }
+}
+
+const onEditingTaskBlure = () => {
+  render()
 }
 
 const changeCheckAll = (event) => {
-  taskArray.forEach(task => {
-    task.isChecked = event.target.checked
-  })
-  render()
+    taskArray.forEach(task => {
+        task.isChecked = event.target.checked
+    })
+    render()
 }
 
 const deleteAllCheckedTasks = () => {
-  taskArray = taskArray.filter(task => task.isChecked != true)
-  
-  render()
+    taskArray = taskArray.filter(task => task.isChecked != true)
+    render()
 }
+
 
 const render = () => {
     let taskElement = ''
+    let isAllSelected = (taskArray.length > 0)
+    let checkedCount = 0
 
     taskArray.forEach(task => {
-        //console.log(task.name)
+        if(!task.isChecked){
+            isAllSelected = false
+        }
+
+        
+        if(task.isChecked){
+          checkedCount++
+        }
 
         taskElement += `
         <li class="todo-item" id="${task.id}">
-            <input type="checkbox" class="todo-checkbox" ${task.isChecked ? 'checked' : ''} class="todo-checkbox">
+            <input type="checkbox" class="todo-checkbox" ${task.isChecked ? 'checked' : ''}>
+            <input type="text" class="todo-input" hidden value="${task.name}"/>
             <div class="todo-text">${task.name}</div>
             <button class="todo-delete">X</button>
         </li>
         `
-        
     })
     taskList.innerHTML = taskElement
+
+    selectAll.checked = isAllSelected
+
+    buttonSortAll.firstChild.data = `All (${taskArray.length})`
+    sortButtonComplited.firstChild.data = `Completed (${checkedCount})`
+    sortButtonActive.firstChild.data = `Active (${taskArray.length - checkedCount})`
+    
 }
 
+render()
+
 buttonAdd.addEventListener("click", addTask)
+
 taskList.addEventListener("click", handleClick)
+taskList.addEventListener("keydown", editingTask)
+taskList.addEventListener("focusout", onEditingTaskBlure)
+
 
 selectAll.addEventListener("change", changeCheckAll)
 buttonDeleteAll.addEventListener("click", deleteAllCheckedTasks)
-
-
-//sortButtonAll.addEventListener("click",)
-//sortButtonActive.addEventListener("click",)
-//sortButtonComplited.addEventListener("click",)
