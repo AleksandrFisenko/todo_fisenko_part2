40cc65d2df4c1733550125ca9ab67e658ad84eda
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2024-06-21 14:28:55 +0300

    обед

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const ENTER = "Enter"
 const ESCAPE = "Escape"
+const TASK_PER_PAGE = 5
 
 const inputTaskName = document.querySelector("#input-name")
 const buttonAdd = document.querySelector("#button-add-todo")
 const selectAll = document.querySelector("#select-all")
 const taskList = document.querySelector("#task-list")
 const buttonDeleteAll = document.querySelector("#button-delete")
+const filterContainer = document.querySelector("#filter-container")
 
-const buttonSortAll = document.querySelector("#button-sort-all")
-const sortButtonActive = document.querySelector("#button-sort-active")
-const sortButtonComplited = document.querySelector("#button-sort-complited")
+const paginationContainer = document.querySelector("#pagination-container")
+
 
 let taskArray = []
 taskArray.push(
@@ -19,6 +20,8 @@ taskArray.push(
         isChecked: true,
     }
 )
+let filterType = "all"
+let currentPage = 0
 
 const addTask = () => {
   if(inputTaskName.value.trim() !== ""){
@@ -34,31 +37,31 @@ const addTask = () => {
 }
 
 const handleClick = (event) => {
-    const clickedItemName = event.target.classList.value
-    const taskId = event.target.parentElement.id
-    if (clickedItemName == "todo-delete") {
-        taskArray = taskArray.filter(task => task.id != taskId)
-        render()
-    } 
-    if(clickedItemName == "todo-checkbox"){
-        taskArray.forEach(task => {
-            if(taskId == task.id){
-                task.isChecked = event.target.checked
-            }
-        })
-        render()
-    }
-    if ((event.detail == 2) && (clickedItemName == "todo-text")) {
+  const clickedItemName = event.target.classList.value
+  const taskId = event.target.parentElement.id
+  if (clickedItemName == "todo-delete") {
+      taskArray = taskArray.filter(task => task.id != taskId)
+      render()
+  } 
+  if(clickedItemName == "todo-checkbox"){
+      taskArray.forEach(task => {
+          if(taskId == task.id){
+              task.isChecked = event.target.checked
+          }
+      })
+      render()
+  }
+  if ((event.detail == 2) && (clickedItemName == "todo-text")) {
 
-      event.target.hidden = true
-      event.target.previousElementSibling.hidden = false
-      event.target.previousElementSibling.focus()
+    event.target.hidden = true
+    event.target.previousElementSibling.hidden = false
+    event.target.previousElementSibling.focus()
 
-    }
+  }
 }
 
-const editingTask = (event) => {
-  console.log(event.key)
+const editTask = (event) => {
+  //console.log(event.key)
   if((event.key == ENTER) && (event.target.value.trim() !== "")){
     const taskId = event.target.parentElement.id
     
@@ -90,39 +93,77 @@ const deleteAllCheckedTasks = () => {
     render()
 }
 
+const filterTasks = (event) => {
+  if(event.target.tagName === "BUTTON"){
+    //console.log(event)
+    Array.from(event.target.parentElement.children).forEach(element => {
+      element.className = "bottom-menu-unselected"
+    })
+    event.target.className = "bottom-menu-selected"
+    filterType = event.target.id
+  }
+  render()
+}
+
+const getFilteredArray = () => {
+  if(filterType === "complited"){
+    return taskArray.filter(task => task.isChecked)
+  }else if(filterType === "active"){
+    return taskArray.filter(task => !task.isChecked)
+  } else{
+    return taskArray
+  }
+}
+
+const updateCheckAll = () => {
+  selectAll.checked = taskArray.every(task => (task.isChecked)) && taskArray.length
+}
+
+const updateFilterContainer = () => {
+  const selectedCount = taskArray.filter(task => task.isChecked).length;
+
+  filterContainer.children[0].firstChild.data = `All (${taskArray.length})`
+  filterContainer.children[1].firstChild.data = `Active (${taskArray.length - selectedCount})`
+  filterContainer.children[2].firstChild.data = `Completed (${selectedCount})`
+}
+
+const getCurrentPage = () => {
+  return getFilteredArray().slice(0, 5)
+}
+
+const renderPagination = () => {
+  let paginationElements = ''
+  for(let i=0; i < Math.ceil(getFilteredArray().length/TASK_PER_PAGE); i++){
+    paginationElements += `
+    <button 
+    class="${(currentPage===i) ? "page-number-selected" : "page-number-unselected"}">
+    ${i+1}
+    </button>
+    `
+  }
+  paginationContainer.innerHTML = paginationElements
+}
 
 const render = () => {
-    let taskElement = ''
-    let isAllSelected = (taskArray.length > 0)
-    let checkedCount = 0
+  let taskElement = ''
+  updateCheckAll()
+  updateFilterContainer()
+  renderPagination()
+  
+  const renderArray = getCurrentPage()
 
-    taskArray.forEach(task => {
-        if(!task.isChecked){
-            isAllSelected = false
-        }
+  renderArray.forEach(task => {
 
-        
-        if(task.isChecked){
-          checkedCount++
-        }
-
-        taskElement += `
-        <li class="todo-item" id="${task.id}">
-            <input type="checkbox" class="todo-checkbox" ${task.isChecked ? 'checked' : ''}>
-            <input type="text" class="todo-input" hidden value="${task.name}"/>
-            <div class="todo-text">${task.name}</div>
-            <button class="todo-delete">X</button>
-        </li>
-        `
-    })
-    taskList.innerHTML = taskElement
-
-    selectAll.checked = isAllSelected
-
-    buttonSortAll.firstChild.data = `All (${taskArray.length})`
-    sortButtonComplited.firstChild.data = `Completed (${checkedCount})`
-    sortButtonActive.firstChild.data = `Active (${taskArray.length - checkedCount})`
-    
+      taskElement += `
+      <li class="todo-item" id="${task.id}">
+          <input type="checkbox" class="todo-checkbox" ${task.isChecked ? 'checked' : ''}>
+          <input type="text" class="todo-input" hidden value="${task.name}"/>
+          <div class="todo-text">${task.name}</div>
+          <button class="todo-delete">X</button>
+      </li>
+      `
+  })
+  taskList.innerHTML = taskElement
 }
 
 render()
@@ -130,9 +171,15 @@ render()
 buttonAdd.addEventListener("click", addTask)
 
 taskList.addEventListener("click", handleClick)
-taskList.addEventListener("keydown", editingTask)
+taskList.addEventListener("keydown", editTask)
 taskList.addEventListener("focusout", onEditingTaskBlure)
 
 
 selectAll.addEventListener("change", changeCheckAll)
 buttonDeleteAll.addEventListener("click", deleteAllCheckedTasks)
+
+filterContainer.addEventListener("click", filterTasks)
+//paginationContainer.addEventListener("click", switchPage)
+
+
+// slice 
